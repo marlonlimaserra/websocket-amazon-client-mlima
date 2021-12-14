93b54eb9824bda8c2571a9f0d04c36a756d1e500
Marlon Lima <marlon@cs.plus>
2021-12-14 14:59:16 -0300

    corrige ping pong

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const socket = ((conf) => {
 
     });
 
-    socket.addEventListener('message',(data) => { if(data === "ping" || data === "pong"){ return false; } filterEvents(data.data); });
+    socket.addEventListener('message',(data) => { if(data.data === "ping" || data.data === "pong"){ return false; } filterEvents(data.data); });
     socket.addEventListener('onclose',(data) => { filterEvents(data.data); });
     socket.addEventListener('onerror',(data) => { filterEvents(data.data); });
     socket.addEventListener('onopen',(data) => { filterEvents(data.data); });
